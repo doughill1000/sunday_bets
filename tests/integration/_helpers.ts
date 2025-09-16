a23bc7c6c0c3709b9858ab83292ec0861926585c
Doug Hill <doughill1000@gmail.com>
2025-09-15 20:05:59 -0400

    hardcode

--- tests/integration/_helpers.ts
+++ tests/integration/_helpers.ts
@@ -1,19 +1,18 @@
 import type { Database } from '$lib/server/db/types';
 import { createClient } from '@supabase/supabase-js';
 
-const serviceKey = process.env.SERVICE_ROLE_KEY;
-
-if (!serviceKey) {
-  throw new Error('Supabase URL or Service Key is missing. Ensure your test environment is configured.');
-}
-
 /**
  * A Supabase client instance authenticated with the service_role key.
  * Use this for all integration test interactions with the database.
  */
-export const createSupaClient = () => createClient<Database>("http://127.0.0.1:54321", serviceKey, {
-  auth: {
-    autoRefreshToken: false,
-    persistSession: false,
-  },
-});
+export const createSupaClient = () =>
+  createClient<Database>(
+    'http://127.0.0.1:54321',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU',
+    {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false
+      }
+    }
+  );
